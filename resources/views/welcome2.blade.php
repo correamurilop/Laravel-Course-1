67088a1142f4f749557144c75ba3397e1b8efee1
Murilo Corrêa <43326750+correamurilop@users.noreply.github.com>
2020-07-17 15:17:15 -0300

    Changing start page and register a new 1 in route

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Teste Murilo
+                    WELCOME 2
                 </div>
 
                 <div class="links">
